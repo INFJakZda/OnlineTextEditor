db75d5d8c3a2d0879915c4f2c733c229d09b07d6
inf127195 <inf127195@mail.com>
2018-01-20 01:03:04 +0100

    Update project Fix errors Change project structure

--- client/main.hpp
+++ client/main.hpp
@@ -14,7 +14,6 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-
 #include <sys/stat.h>
 #include <time.h>
 #include <signal.h>
@@ -24,5 +23,21 @@
 #define PAGE_Y 45
 
 using namespace std;
+
+extern int clientSPECIAL_ID;
+extern int socketDescE;
+extern int socketDescA;
+extern string servIPaddr;
+extern int servPORT_E;
+extern int servPORT_A;
+extern bool end_program_e;
+extern bool end_program_a;
+extern bool reconnect_ed;
+extern bool reconnect_acv;
+extern struct tm *foo;
+extern struct stat attrib;
+
+void manage_editor();
+void manage_activ();
 
 #endif
