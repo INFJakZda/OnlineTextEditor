63469abee14c175708faa8724281247d4f2038a3
inf127195 <wojciech_jan.sokolowski@student.put.poznan.pl>
2018-02-10 17:55:45 +0100

    Upload New File

---
+++ client/mainwindow.h
@@ -0,0 +1,42 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDebug>
+
+#include <string>
+
+#define FLAG_UPDATE_FROM_SERV 1
+
+extern std::ofstream logFile;
+extern std::string dataFromQTextEdit;
+extern std::string dataFromServer;
+extern volatile bool isEndProgram;
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+signals:
+    void messageSent(int);
+
+public slots:
+    void sendMessage(int);
+
+private slots:
+    void onTextChanged();
+    void internalMessage(int);
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
